21fa6b4224a47877abc3366fac9e39eefa3b7940
Benoit Beausejour <b@turblent.ca>
2024-03-05 16:56:17 -0500

    chore: ci

--- ci/docker.sh
+++ ci/docker.sh
@@ -18,17 +18,13 @@ fi
 echo "Creating the Docker builder"
 docker buildx create --name services-builder --driver docker-container --use --bootstrap
 
-# Login to ECR
-aws ecr get-login-password --region "us-east-1" | docker login --password-stdin --username AWS ${DOCKER_ECR}
-
 # We build Arm and x64 images and push them to ECR and Turbulent's registry
 export DOCKER_BUILD_CMD="docker buildx build --platform linux/amd64,linux/arm64 --progress plain --push"
 
 # Build the main image
 echo "Building the base image"
 time ${DOCKER_BUILD_CMD} -f docker/Dockerfile \
-  -t ${HARBOR_REGISTRY}/${DOCKER_NAMESPACE}/${BASE_IMAGE}:${IMAGE_TAG} \
-  -t ${DOCKER_ECR}/${BASE_IMAGE}:${IMAGE_TAG} .
+  -t ${HARBOR_REGISTRY}/${DOCKER_NAMESPACE}/${BASE_IMAGE}:${IMAGE_TAG}
 
 
 if [[ ${CI_JOB_MANUAL} == "true" ]]; then
